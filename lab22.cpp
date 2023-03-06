ed7ddb0f7d2745f180f7665f4dc0a8656d583490
Goodl3oyZ <panudetzx2@gmail.com>
2023-03-07 04:49:21 +0700

    sed la

--- lab22.cpp
+++ lab22.cpp
@@ -1,4 +1,4 @@
-// [Missing Code 1] Include header file.
+#include "lab22.h"
   
 int main(){
 	srand(time(0));
@@ -9,7 +9,10 @@ int main(){
 	Unit hero("Hero",name);
 	
 	Equipment sword(0,8,4);
-	// [Missing Code 2]  Create Equipment axes, shield and armor here
+	Equipment axes(0,16,-3);
+	Equipment shield(0,-1,7);
+	Equipment armor(25,-2,2);
+	
 
 	
 	
@@ -17,7 +20,10 @@ int main(){
 	cout << " [1] Sword \n [2] Axes \n [3] Shield \n [4] Armor \n";
 	cout << "Please selet your equipment: ";
 	cin >> eq;
-	// [Missing Code 3] Equip a selected equipment to the hero. 
+	if(eq == '1') hero.equip(&sword);
+	if(eq == '2') hero.equip(&axes);
+	if(eq == '3') hero.equip(&shield);
+	if(eq == '4') hero.equip(&armor);
 
 	
 	
@@ -58,7 +64,10 @@ int main(){
 			cout << "Please select your equipment: ";
 			cin >> eq;
 			// [Missing Code 3] Equip a selected equipment to the hero.
-
+			if(eq == '1') hero.equip(&sword);
+			if(eq == '2') hero.equip(&axes);
+			if(eq == '3') hero.equip(&shield);
+			if(eq == '4') hero.equip(&armor);
 			
 		}
 		
